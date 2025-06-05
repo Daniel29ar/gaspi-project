b23bc2d23950f6dc109e6e34e761b3a562461344
Daniel29ar <daniel1994ar@gmail.com>
2025-06-05 09:03:58 -0500

    add empty project

---
+++ GaspiPRoject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GaspiPRoject
+//
+//  Created by Daniel Enrique Vergara Cornelio on 5/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
